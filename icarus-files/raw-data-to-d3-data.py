c78c1881654a0ba4f67a0985daee11453cc44d2d
Connor St Clair <connor.stclair@protonmail.com>
2020-05-06 18:15:42 +1000

    before cleanup - but it works for varying levels from bootnodes

--- icarus-files/raw-data-to-d3-data.py
+++ icarus-files/raw-data-to-d3-data.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from collections import deque
 
 
 BOOT_NODES = set(["18.138.108.67", "3.209.45.79", "34.255.23.113", "35.158.244.151", "52.187.207.27", "191.234.162.198", "52.231.165.108", "104.42.217.25"])
@@ -28,6 +29,18 @@ def get_all_ips(data):
             result.add(neighbour_ip)
     return result
 
+def get_adj_list(filename):
+    with open(filename,"r") as f:
+        data = json.load(f)
+        nodes = data["nodes"]
+        result =  {v: [] for v in get_all_ips(data)}
+        for node in nodes:
+            ip = node["ip"]
+            for neighbour in node["neighbours"]:
+                result[ip].append(neighbour)
+        return result
+
+
 def data_to_graph_data(filename):
     with open(filename,"r") as f:
         data = json.load(f)
@@ -51,7 +64,6 @@ def data_to_graph_data(filename):
                     result["nodes"].append(create_node(neighbour, 2))
     write_json_to_file("graph-data-d3-ready.json", result)
 
-
 def data_to_graph_data_from_boot_nodes(filename, levels):
     with open(filename,"r") as f:
         data = json.load(f)
@@ -61,17 +73,27 @@ def data_to_graph_data_from_boot_nodes(filename, levels):
         result["nodes"] = create_boot_nodes()
         result["links"] = []
 
-        seen_before = set(list(BOOT_NODES))
-        to_visit = set()
-        for node in nodes:
-            if node["ip"] in BOOT_NODES:
-                for neighbour in node["neighbours"]:
-                    if neighbour not in seen_before:
-                        result["nodes"].append(create_node(neighbour, 2))
-                        seen_before.add(neighbour)
-                    result["links"].append(create_link(node["ip"], neighbour))
-        write_json_to_file("graph-data-d3-ready-boot-nodes.json", result)
-   
+
+        adj_list = get_adj_list(filename)
+        
+        visited = set(list(BOOT_NODES))
+        q = deque(list(BOOT_NODES))
+        final_node = q[-1]
+        while len(q) > 0:
+            ip = q.popleft()
+            for neighbour in adj_list[ip]:
+                if neighbour not in visited:
+                    result["nodes"].append(create_node(neighbour, 2))
+                    q.append(neighbour)
+                    visited.add(neighbour)
+                result["links"].append(create_link(ip, neighbour))
+            if ip == final_node:
+                levels -= 1
+                if levels == 0 or len(q) == 0:
+                    break
+                final_node = q[-1]
+        return result
 
 if __name__ == "__main__":
-    data_to_graph_data_from_boot_nodes("graph-data-raw.json",1)
+    result = data_to_graph_data_from_boot_nodes("graph-data-raw.json",6)
+    write_json_to_file("graph-data-d3-ready-boot-nodes.json", result)
